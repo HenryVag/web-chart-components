d3a2f42e89af8f891d09c4ea0ee702de5b3922bc
HenryVag <146335816+HenryVag@users.noreply.github.com>
2026-03-16 16:34:00 +0200

    feat: add piechart to app

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,28 @@
 import "./App.css"
+import Piechart from "./charts/piechart/piechart"
+import { ExampleLegend } from "./charts/utils/default-props"
+import testData from "./charts/utils/test-data"
 
 function App() {
-	return <h1 className="app-header">React Charts</h1>
+	return (
+		<div>
+			<h1 className="app-header">React Charts</h1>
+			<Piechart
+				data={testData}
+				radius={20}
+				showLabels={true}
+				labelDistance={1.3}
+				stroke="black"
+				strokeWidth={0.5}
+				otherSectorLabel="Muut"
+				sectorStrokeWidthThreshold={90}
+				sectorStrokeWidth={3}
+				legend={(data) => <ExampleLegend data={data} />}
+				legendPosition="right"
+				title="piirakkakaavio"
+			/>
+		</div>
+	)
 }
 
 export default App
